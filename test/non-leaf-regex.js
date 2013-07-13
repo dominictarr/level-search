1e64c14c2d395b6fca53ace88cb6e14ba5136f45
James Halliday <mail@substack.net>
2013-07-12 23:21:26 -0700

    failing test for non-leaf regex keys

---
+++ test/non-leaf-regex.js
@@ -0,0 +1,60 @@
+var level    = require('level-test')()
+var sublevel = require('level-sublevel')
+var pull     = require('pull-stream')
+
+var tape     = require('tape')
+var search   = require('../')
+
+var db = sublevel(level('level-search--non-leaf-regex', {encoding: 'json'}))
+var index = search(db, 'index')
+
+tape('init', function (t) {
+  db.batch([
+    {
+      key: 'dominictarr',
+      value: {location: {country: 'new zealand', city:'aukland'}},
+      type: 'put'
+    },
+    {
+      key: 'substack',  
+      value: {location: {Country: 'usa', City: 'oakland', State: 'california' }},
+      type: 'put'
+    },
+    {
+      key: 'hank',  
+      value: {location: {country: 'usa', state: 'maryland'}},
+      type: 'put'
+    },
+    {
+      key: 'maxogden',
+      value: {location: {country: 'usa', city: 'Oakland'}},
+      type: 'put'
+    },
+    {
+      key: 'chrisdickinson',
+      value: {location: {country: 'usa', city: 'portland'}},
+      type: 'put'
+    },
+    {
+      key: 'raynos',
+      value: {location: {country: 'usa', city: 'san francisco'}},
+      type: 'put'
+    }
+  ], function (err) {
+    if(err) throw err
+    t.end()
+  })
+})
+
+tape('multiple regex search', function (t) {
+  t.plan(1)
+  console.log(index.explain(['location', /^(city|state)$/i, /land$/i]))
+
+  pull(
+    index.search([ 'location', /state/i ]),
+    pull.collect(function (err, matches) {
+      var names = matches.map(function (row) { return row.key })
+      t.deepEqual(names.sort(), [ 'hank', 'substack' ])
+    })
+  )
+})
